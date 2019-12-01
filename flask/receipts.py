c567f75785e988fd79ef5e636bebbf1a8b5511b0
Christopher Hui <christophergkhui@gmail.com>
2019-11-30 18:52:46 -0800

    Added vision API calls which retrieves the value of the total

--- flask/receipts.py
+++ flask/receipts.py
@@ -3,4 +3,4 @@ import os
 import flask
 
 print(os.path.join(os.path.dirname(__file__), 'lib'))
-print(os.path.join(os.path.dirname(__file__), '../venv/Lib'))
+# print(os.path.join(os.path.dirname(__file__), '../venv/Lib'))
